bacfdad84f4ec123a713bce23775d8c476bd4148
Tomas Laus <tomaslaus99@gmail.com>
2024-03-17 19:17:10 -0300

    <add> setup multer

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
+var multer = require('multer');
+
 
 var app = express();
 
@@ -10,6 +12,9 @@ app.use('/public', express.static(process.cwd() + '/public'));
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
+
+// Setup multer
+const upload = multer({ storage: multer.memoryStorage() });
 
 
 
